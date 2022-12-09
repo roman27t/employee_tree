97884908b3145467b47ccd044f8dfe634835a0f2
roman <roman.teterin.01@privatbank.ua>
2022-12-09 09:57:05 +0200

    update: conftest

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -6,13 +6,12 @@ from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
+from config import i_config
 from main import app_factory
 from models import Base
 from tools.init_data_db import init_data
 
-_T_DB_NAME = 'postgres_test'
-_T_DB_URL=f'postgresql+asyncpg://postgres:postgres@pg_db-test/{_T_DB_NAME}'
-engine = create_async_engine(_T_DB_URL, echo=True)
+engine = create_async_engine(i_config.DB_URL, echo=True)
 
 
 @pytest.fixture(scope="session")
@@ -34,10 +33,10 @@ def event_loop():
 @pytest.fixture
 async def create_test_client(aiohttp_client):
     async with engine.begin() as conn:
-        if _T_DB_NAME in engine.url:
+        if i_config.DB_URL.split('/')[-1] == 'postgres_test':
             await conn.execute(text('CREATE EXTENSION IF NOT EXISTS ltree;'))
             await conn.run_sync(Base.metadata.drop_all)
             await conn.run_sync(Base.metadata.create_all)
     async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
     await init_data(sa_session=async_session())
-    return await aiohttp_client(await app_factory(db_url=_T_DB_URL))
+    return await aiohttp_client(await app_factory(db_url=i_config.DB_URL))
