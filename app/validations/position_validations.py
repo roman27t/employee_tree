2f5764ee633d3e246934a3960903aafb62a9654e
roman <roman.teterin.01@privatbank.ua>
2022-12-11 20:10:15 +0200

    add: _PostPositionBaseValidate

--- app/validations/position_validations.py
+++ app/validations/position_validations.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import Optional
 
 from models import PositionModel
@@ -6,47 +7,46 @@ from tools.exceptions import InValidException
 from validations.abstract_validations import ValidateAbstract
 
 
-class PostPositionValidate(ValidateAbstract):
+class _PostPositionBaseValidate(ValidateAbstract, ABC):
     def init(self):
-        self.__obj: Optional[PositionModel] = None
+        self._obj: Optional[PositionModel] = None
 
     @property
     def obj_model(self) -> PositionModel:
-        return self.__obj
+        return self._obj
 
+    @abstractmethod
+    def _set_input_data(self):
+        pass
+
+    def _sync_validations(self) -> tuple:
+        return (self._set_input_data,)
+
+    
+class PostPositionValidate(_PostPositionBaseValidate):
     @property
     def input_schema(self) -> PostPositionSchema:
         return self.__input_schema
 
     def _sync_validations(self) -> tuple:
-        return (self.__set_input_data,)
+        return (self._set_input_data,)
 
-    def __set_input_data(self):
+    def _set_input_data(self):
         self.__input_schema = PostPositionSchema.parse_custom(data=self.body, code='bad_schema')
 
 
-class PatchPositionValidate(ValidateAbstract):
-    def init(self):
-        self.__obj: Optional[PositionModel] = None
-
+class PatchPositionValidate(_PostPositionBaseValidate):
     @property
     def input_schema(self) -> PatchPositionSchema:
         return self.__input_schema
 
-    @property
-    def obj_model(self) -> PositionModel:
-        return self.__obj
-
-    def _sync_validations(self) -> tuple:
-        return (self.__set_input_data,)
-
     def _async_validations(self) -> tuple:
         return self.__validate_obj,
 
-    def __set_input_data(self):
+    def _set_input_data(self):
         self.__input_schema: PatchPositionSchema = PatchPositionSchema.parse_custom(data=self.body, code='bad_schema')
         if not self.__input_schema.has_values():
             raise InValidException(status_code=400, code='nothing_update')
 
     async def __validate_obj(self):
-        self.__obj = await self._get_db_obj(class_model=PositionModel, pk=self.id_schema.id, code='bad_object')
+        self._obj = await self._get_db_obj(class_model=PositionModel, pk=self.id_schema.id, code='bad_object')
