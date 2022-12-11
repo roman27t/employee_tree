f3b427bd604dea42e0a91f11b15610c273dbc8f0
roman <roman.teterin.01@privatbank.ua>
2022-12-11 13:46:53 +0200

    add: init_db to template

--- app/routers.py
+++ app/routers.py
@@ -8,10 +8,10 @@ def get_routes() -> list:
         web.get('/', StaffTemplateView, name='index'),
         web.get('/staff/', StaffView, name='staff_tree'),
         web.get('/staff/{id}/', StaffView, name='staff'),
-        web.post('/staff/', StaffView),
-        web.patch('/staff/{id}/', StaffView),
+        web.post('/staff/', StaffView, name='staff_post'),
+        web.patch('/staff/{id}/', StaffView, name='staff_patch'),
         web.get('/position/', PositionView, name='positions'),
         web.get('/position/{id}/', PositionView, name='position'),
         # service urls
-        web.get('/system/init_data/', init_data_view),
+        web.get('/system/init_data/', init_data_view, name='init_db'),
     ]
